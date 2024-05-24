0fd1d005c566cfa648c3d7358eea71730c623c84
Nick Nick <piratescreen@gmail.com>
2024-05-24 09:43:18 +0300

    Task-1 Finish

---
+++ C-Sharp-Apps-Nikita/BankApp/BankAppMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Apps_Nikita.BankApp
+{
+    public class BankAppMain
+    {
+        public static void MainEntry()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Name of application is BankApp");
+            Console.WriteLine("How Can I Help?");
+            Console.WriteLine();
+        }
+    }
+}
